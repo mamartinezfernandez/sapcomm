723cabc460582a12d077115ee51d4cdd2e19af6f
mmartinezfernandez <37104570+mmartinezfernandez@users.noreply.github.com>
2021-11-23 19:02:17 +0100

    Update setNumRoundCust.groovy

--- groovys/setNumRoundCust.groovy
+++ groovys/setNumRoundCust.groovy
@@ -1,1 +1,30 @@
+import com.sap.it.api.mapping.*;
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+import groovy.xml.MarkupBuilder;
+import groovy.xml.*;
 
+def String customFunc(String arg1, MappingContext context){
+  
+  if(arg1 != '' && arg1 != null){
+    
+    String xx = arg1
+    int ind = xx.indexOf(".")
+    ind = ind+1
+    String xxs = xx.substring(ind)
+    
+    if(xxs.length >= 3){
+      String xxl = xxs.substring(2)
+    }
+    
+    if(xxl >= '5' && xxl <= '9'){
+      float x = Float.parseFloat(xx)
+      x = x + 0.001
+      x = x.round(2)
+    }
+    
+    String val1 = x
+    return val1
+    
+  } 
+}
