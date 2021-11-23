8a5e19c0ea5440e14bd7583db7731acd140614b5
mmartinezfernandez <37104570+mmartinezfernandez@users.noreply.github.com>
2021-11-23 18:03:26 +0100

    Create setNumRound.groovy

---
+++ groovys/setNumRound.groovy
@@ -0,0 +1,21 @@
+import com.sap.it.api.mapping.*;
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+import groovy.xml.MarkupBuilder;
+import groovy.xml.*;
+
+def String customFunc(String arg1, MappingContext context){
+  
+  if(arg1 != '' && arg1 != null{
+    
+    String xx = arg1
+  
+    float x = Float.parseFloat(xx);
+    float a = x.round(2)
+  
+    String aa = a
+  
+    return a
+   
+  } 
+}
