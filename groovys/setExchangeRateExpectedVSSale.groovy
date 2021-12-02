fe741c399300d937da85bd1fb5efb2e255667acc
mmartinezfernandez <37104570+mmartinezfernandez@users.noreply.github.com>
2021-12-02 17:42:25 +0100

    Create setExchangeRateExpectedVSSale.groovy

---
+++ groovys/setExchangeRateExpectedVSSale.groovy
@@ -0,0 +1,30 @@
+def String customFunc(String arg1, MappingContext context){
+    
+    def body = context.getProperty('B1');
+    def xml = new XmlSlurper().parseText(body);
+
+    String curr1 = ''
+    String exchangeRate1 = ''
+
+    def value1 = xml.terminalFinaliseTransaction.cmTenderItem.find{ cmTenderItem ->
+        cmTenderItem.itemType.text() == arg1       
+    }
+
+    curr1 = value1.currency
+    exchangeRate1 = value1.exchangeRate
+
+    if(value1==null){
+        return 'no exists'
+    }
+    else{
+        def value2 = xml.terminalFinaliseTransaction.cmTenderItem.find{ cmTenderItem ->
+            cmTenderItem.itemType.text() == 'Sale' && cmTenderItem.currency.text() == curr1            
+        }
+        if(value2==null){
+            return 'no exists'
+        }
+        else{
+            return exchangeRate1
+        }
+    }             
+}
